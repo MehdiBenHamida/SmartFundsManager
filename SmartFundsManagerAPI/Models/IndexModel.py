9a93898e97aa67e6efa930e04ee343b58abcc7e3
Mehdi Ben Hamida <mehdi.benhamida@gmail.com>
2019-03-27 10:13:54 +0100

    [WIP]: Project format

--- SmartFundsManagerAPI/Models/IndexModel.py
+++ SmartFundsManagerAPI/Models/IndexModel.py
@@ -9,3 +9,21 @@ class Index(db.model):
     # TODO: complete the class definition and complete the docstring
 
     pass
+
+
+# TODO: do not commit this for the first project format
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+    
